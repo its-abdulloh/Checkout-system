285e8e0dee5adf1ea38bb8a26605662dff900287
Abdulloh <foziljonovabdulloh64@gmail.com>
2026-06-02 18:44:39 +0500

    Added checkout page that scans barcodes and retrieves data

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def index():
     return render_template("index.html")
 
 
-
 @app.route("/add",methods=["GET","POST"])
 def add():
     if request.method == "POST":
@@ -51,3 +50,49 @@ def add():
 
     return render_template("add.html",success=success,failed=failed)
         
+
+@app.route("/checkout",methods=["GET","POST"])
+def checkout():
+    conn = sqlite3.connect("store.db")
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    if "cart" not in session:
+        session["cart"] = {}
+
+    if request.method =="POST":
+        barcode = request.form.get("barcode")
+        product = cursor.execute("SELECT id, name, price, stock FROM products WHERE barcode = ?",(barcode,)).fetchone()
+
+        if product:
+            cart = session["cart"]
+
+            product_id = str(product["id"])
+            if product_id in cart:
+                cart[product_id]["quantity"]+=1
+                return redirect("/checkout")
+            else:
+                cart[product_id] = {
+                    "name":product["name"],
+                    "price":product["price"],
+                    "stock":product["stock"],
+                    "quantity":1
+                }
+                session["cart"] = cart
+                return redirect("/checkout")
+        else:
+            return render_template("checkout.html",error = "Mahsulot topilmadi!")
+
+    total = 0
+    cart = session["cart"]
+    if cart:
+        for product in session["cart"].values():
+            total += product["price"]*product["quantity"]
+
+    conn.close()
+
+    return render_template("checkout.html",cart = cart.values(),total=total)
+
+
+# @app.route("/sell",methods=["POST"])
+# def sell():
---
+++ static/app.js
@@ -0,0 +1,3 @@
+document.querySelector('input[name="barcode"]').addEventListener('blur', function() {
+    this.focus();
+});
